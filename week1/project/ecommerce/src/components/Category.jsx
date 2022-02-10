9801819a853b7fc74d7800f2f67cea0019383f53
Reema <Reemaalshohof88@gmail.com>
2022-02-10 15:35:26 +0100

    add css class to Category button for hover effect

--- week1/project/ecommerce/src/components/Category.jsx
+++ week1/project/ecommerce/src/components/Category.jsx
@@ -3,7 +3,7 @@ import "./Css/category.css";
 const Category = ({ isActiveStyle, ChangeCategory, Value }) => {
   return (
     <button
-      className={` f5  ph3 ma1 pv2 mb2 dib black b ${isActiveStyle}`}
+      className={`category f5  ph3 ma1 pv2 mb2 dib black b ${isActiveStyle}`}
       onClick={ChangeCategory}
       value={Value}
     >
